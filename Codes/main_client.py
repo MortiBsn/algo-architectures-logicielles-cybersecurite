32093499901048bf134059791e07c9ff873b4d9a
MortiBsn <mortimer.bisin@student.hepl.be>
2024-11-24 22:54:52 +0100

    4th exercices done

--- Codes/main_client.py
+++ Codes/main_client.py
@@ -1,24 +1,36 @@
 from client import Client
 import des
-import aes
+from aes import Aes
 import time
+from HmacMD5 import HmacMd5
 
-def sending_message(client):
-    client.connect()
-    client.txt = "I am the message hello world "
-    client.mode.encrypt(client.txt)
-    print(client.txt)
-    client.send_data()
 
 des = des.Des()
 client = Client(des)
-sending_message(client)
+client.connect()
+client.txt = "I am the message hello world "
+client.mode.encrypt(client.txt)
+print(client.txt)
+client.send_data()
+
 
 # Waiting one second so no port prob
 time.sleep(1)
-
-aes = aes.Aes()
+aes = Aes()
 client2 = Client(aes)
-sending_message(client2)
+client2.txt = "I am the message hello world "
+client2.connect()
+client2.send_data_aes()
+
+time.sleep(1)
+
+
+client3 = Client(None)
+client3.connect()
+client3.txt = "I am the message hello world "
+client3.send_data_HmacMd5("ma_clé_secrète")
+
+
+
 
 # connection
